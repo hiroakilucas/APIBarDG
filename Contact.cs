9e8732fe2ce96fd1beea417fb98dfef966b8bdc1
hiroakilucas <hiroaki.lucas@gmail.com>
2020-07-25 22:29:49 -0300

    Initial commit

---
+++ Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace APIBarDG
+{
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Info.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace APIBarDG
+{
+    internal class Info : OpenApiInfo
+    {
+        public string Title { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public object Contact { get; set; }
+    }
+}
